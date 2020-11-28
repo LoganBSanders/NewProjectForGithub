0c34241983bd2229c1799829bce2b8b227a177dc
LoganBSanders <loganbsanders@gmail.com>
2020-11-27 18:43:33 -0600

    Changed the name

--- src/GitHubProject.java
+++ src/GitHubProject.java
@@ -2,8 +2,8 @@ public class GitHubProject {
     public static void main(String[] args) {
         System.out.println("Hello Ozan");
         System.out.println("Hello Ozan");
-        System.out.println("Hello Ozan");
-        System.out.println("Hello Ozan");
+        System.out.println("Hello Burak");
+        System.out.println("Hello Logan");
         System.out.println("Hello Ozan");
     }
 }
