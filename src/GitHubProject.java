aff86a9792a72b1dd927036b0391fd8b303b3530
LoganBSanders <loganbsanders@gmail.com>
2020-11-27 18:30:31 -0600

    second commit

--- src/GitHubProject.java
+++ src/GitHubProject.java
@@ -1,5 +1,9 @@
 public class GitHubProject {
     public static void main(String[] args) {
         System.out.println("Hello Ozan");
+        System.out.println("Hello Ozan");
+        System.out.println("Hello Ozan");
+        System.out.println("Hello Ozan");
+        System.out.println("Hello Ozan");
     }
 }
